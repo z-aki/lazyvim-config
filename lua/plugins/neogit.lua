32dfcd4cdcd35dced068c168159dbc686fc575e6
Ankit Meel <ankitjmeel@gmail.com>
2026-03-12 14:51:25 +0530

    neogit, sublime keymap

--- lua/plugins/neogit.lua
+++ lua/plugins/neogit.lua
@@ -15,7 +15,15 @@ return {
     -- "folke/snacks.nvim", -- optional
   },
   cmd = "Neogit",
+  opts = {
+    integrations = {
+      diffview = true,
+      fzf_lua = true,
+    },
+    kind = "auto"
+  },
   keys = {
     { "<leader>gg", "<cmd>Neogit<cr>", desc = "Show Neogit UI" },
+    { "<leader>gP", '<cmd>DiffviewOpen origin/master...HEAD --imply-local<cr>', desc = "Show Diffview UI" },
   },
 }
